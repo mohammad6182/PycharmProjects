9592254aace50fd13dc4cf40715e8d9dc50d5f14
mohammad6182 <104463077+mohammad6182@users.noreply.github.com>
2022-09-16 22:02:54 -0700

    Initial commit

---
+++ DjangoUniversity/campusApp/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+
+# Create model of campus information
+
+class UniversityCampus(models.Model):
+    campus_name = models.CharField(max_length=25, default="", blank=False, null=False)
+    campus_id = models.IntegerField(default="", blank=False, null=False)
+    campus_state = models.CharField(max_length=10, default="", blank=True, null=False)
+
+
+    # Creates model manager
+    object = models.Manager()
+
+
+
+    def __str__(self):
+
+        display_campus = '{0.title}: {0.campus_name}'
+        return display_campus.format(self)
+
+
+    class Meta:
+        verbose_name_plural = "University Campus"
+
+
+
+
